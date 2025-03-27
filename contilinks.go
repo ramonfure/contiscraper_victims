f968526e4b88ac6156bd20e8bbc5997276f2ad58
kanjid <kanjid@MacBook-Pro-de-kanjid.local>
2025-03-27 20:35:36 +0100

    Update createFile() function and fix minor errors

--- contilinks.go
+++ contilinks.go
@@ -1,72 +1,98 @@
 package main
 
 import (
+	//"encoding/json"
+	//"flag"
+
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gocolly/colly"
-	"github.com/gocolly/colly/proxy"
 )
 
+//var path = "/home/kanjid/projects/conticrw/dbconti/conti_1.txt
+//
 const (
-	URL_CONTI = "http://continewsnv5otx5kaoje7krkto2qbu3gtqef22mnr7eaxw3y6ncz3ad.onion"
+      
+      //Conti URL
+      CONTI_URL_ONION = "http://continewsnv5otx5kaoje7krkto2qbu3gtqef22mnr7eaxw3y6ncz3ad.onion"
 
-	TOR1 = "socks5://127.0.0.1:9050"
-	TOR2 = "socks5://127.0.0.1:9051"
-	TOR3 = "socks5://127.0.0.1:9052"
-
-	USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
-)
-
+      //User Agents
+      USER_AGENT_TORBROWSER = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+      
+      //Proxy Tor
+      TOR1 = "socks5://127.0.0.1:9050"
+    
+  )
 func main() {
-	c := colly.NewCollector(
-		colly.UserAgent(USER_AGENT),
-	)
-	rp, err := proxy.RoundRobinProxySwitcher(TOR1, TOR2, TOR3)
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.SetProxyFunc(rp)
 
-	// On every a element script
-	c.OnHTML("script", func(r *colly.HTMLElement) {
+	c := colly.NewCollector()
 
-		fmt.Println("Probando la de conti y su script, visitando", r.Request.URL.String(), "texto...", r.Text)
-	})
+	c.SetProxy(TOR1)
 
-	c.OnHTML("a[href]", func(r *colly.HTMLElement) {
-		r.Request.Visit(r.Attr("href"))
-	})
-
-	/* c.OnRequest(func(a *colly.Request) {
-		fmt.Println("Visiting:", a.URL.String())
+	c.OnHTML("script", func(e *colly.HTMLElement) {
 
 	})
-	*/
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Got a response from", r.Request.URL)
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit(URL_CONTI)
-
-	c.Wait()
+	c.Visit(Abba)
+	fmt.Println("Done!")
+    }
 }
 
-/* func createFile() {
+func crawl(urlSearch string, user_agent string) {
 
-	// If the file doesn't exist, create it, or append to the file
-	f, err := os.OpenFile("conti_.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//if _, err := f.Write([]byte("appended some data\n")); err != nil {
-	if _, err := f.Write([]byte(link)); err != nil {
-		log.Fatal(err)
-	}
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
+  urlConti, _ := url.Parse(CONTI_URL_ONION)
+  
+  //Instantitae new NewCollector
+  c := colly.NewCollector(
+    colly.UserAgent(USER_AGENT_TORBROWSER),
+    colly.AllowedDomains(urlConti.Host),
+    colly.Async(true),
+  )
+  
+  //No revisit
+  c.AllowURLRevisit = false
+  c.SetRequestTimeout(20 * time.Second)
+  
+  //Setting proxy
+  c.SetProxy(TOR1)
+
+
+  c.OnHTML(("a[href]", func(e *colly.HTMLElement))) {
+	link := e.Attr("href")
+	absoluteUrl := e.Request.AbsoluteURL(link)
+
+  }
+  c.OnHTML(("script", func(e *colly.HTMLElement))) {
+        text := e.Text()
+
+  }
+  c.OnRes
+
 
 }
-*/
+
+func createFile(){
+ link := e.Text
+  // If the file doesn't exist, create it, or append to the file
+ f, err := os.OpenFile("conti_bbdd.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+   			log.Fatal(err)
+		}
+  //if _, err := f.Write([]byte("appended some data\n")); err != nil {
+		if _, err := f.Write([]byte(link)); err != nil {
+			log.Fatal(err)
+		}
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+
+
+}
